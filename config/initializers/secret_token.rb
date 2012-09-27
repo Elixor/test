92db775a813d2dad1ecc74316bbd365998f04d0e
elixor <skarab.cz@seznam.cz>
2012-09-27 11:48:19 +0200

    new repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '7183d0b72cc0047a9fdeb7a3e7816b57f6d3cb04681d1de25d43607f0aa975827fa8dab2a54ac0ad54381ffd82ddb279c5fdb1ee4904ec83e220ee2341df5574'
